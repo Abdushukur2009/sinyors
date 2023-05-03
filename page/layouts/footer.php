e6f7b4a18ff3b4d23e5f8dab8455b54fae60709e
Abdushukur2009 <112758065+Abdushukur2009@users.noreply.github.com>
2023-05-03 17:02:07 +0500

    eqw

--- page/layouts/footer.php
+++ page/layouts/footer.php
@@ -5,7 +5,7 @@
                     <a href="#"><i class="fab fa-facebook hover-icon"></i></a>
                     <a href="#"><i class="fab fa-twitter hover-icon mx-5"></i></a>
                     <a href="#"><i class="fab fa-instagram hover-icon mx-5"></i></a>
-                    <a href="#"><i class="fab fa-telegram hover-icon"></i></a><br><br><br>
+                    <a href="https://t.me/super_game_uz_bek"><i class="fab fa-telegram hover-icon"></i></a><br><br><br>
                     <a href="#" class="color-red"><h1>SeniorSite😂</h1></a><br><br>
                 </div>
             </div>
